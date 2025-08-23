10bcf65e54f979bd693f1fb4acdaae5229a63799
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2025-08-23 09:25:57 +0530

    initial commit

---
+++ app/src/main/java/com/rl/taskup/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.rl.taskup
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.rl.taskup.presentation.ui.theme.TaskUpTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaskUpTheme {
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/rl/taskup/presentation/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.rl.taskup.presentation.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
